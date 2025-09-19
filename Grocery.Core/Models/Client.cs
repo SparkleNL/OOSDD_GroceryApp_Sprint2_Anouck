350aba054c3fd8a01c366bf6ba064da89e469ddb
SparkleNL <ajansen1608@gmail.com>
2025-09-19 11:12:49 +0200

    UC6 commit 2 - following instructions
    Step 4 t/m 8.
    Uncommenting comments and adding existing file.

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -3,8 +3,8 @@ namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string EmailAddress { get; set; }
-        private string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             EmailAddress = emailAddress;
